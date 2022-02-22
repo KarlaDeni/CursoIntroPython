c8c2e0d3bfddc8399a6a0106898a1e88ac9b258d
KarlaDeni <kardeni2809@gmail.com>
2022-02-21 20:36:50 -0600

    Terminacion de KATAS

---
+++ Módulo 1 - Primeros pasos/input.py
@@ -0,0 +1,11 @@
+# Escribe tu código aquí
+#* print("Calculadora")
+#first_number = input("Primer numero : ")
+#second_number=input("Segundo numero : ")
+#print(first_number + second_number)
+#print (int(first_number)+int(second_number))
+
+# Escribe tu código aquí
+print("Bienvenido al programa de la bienvenida")
+name=input("Introduzca su nombre")
+print("Saludos :" + name)
